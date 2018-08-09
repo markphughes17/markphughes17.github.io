85c288b4c3126d6be9851ff407cb3fe724bae5e7
mark <markphughes17@hotmail.co.uk>
2018-08-09 22:09:07 +0100

    commit

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -4,10 +4,14 @@ import GooglePhoto from 'react-google-photo';
 
 const images = [
     {
-        src: 'https://photos.app.goo.gl/26EmKugg7YkJW7sc7',
+        src: 'https://images.unsplash.com/photo-1509420316987-d27b02f81864?dpr=1&auto=format&fit=crop&w=1500&q=80&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+        width: 749,
+        height: 1000,
     },
     {
         src: 'https://photos.app.goo.gl/zjSKMN7g2LrcjsLK8',
+        width: 749,
+        height: 1000,
     }
 ];
 
@@ -45,6 +49,7 @@ class Albums extends Component {
                     onClickPrev={this.handleClickPrev}
                     onClickNext={this.handleClickNext}
                     onClose={this.handleClose}
+                    closeOnEsc={true}
                 />
             </div>
         )
