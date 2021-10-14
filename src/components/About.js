d0a4a3ff175296eb972ac790049cd47bd7054434
Mark Hughes <mark.hughes@kirasystems.com>
2021-10-14 11:11:13 +0100

    Added About page with family photo

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <section>
+                <p>WIP</p>
+                <img src="https://drive.google.com/uc?id=1njKlx1wKyZGf5SMZ70fxgB980RB5p0fN" alt="picutre of me and my family" class="album-img"/>
+            
+
+            </section>
+        )
+    }
+}
+
+export default Home;
