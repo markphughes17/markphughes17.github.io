e934d8da80cc77acea3d9afcd62ce4ddc3cdb4c4
Mark Hughes <markphughes17@hotmail.co.uk>
2022-07-13 12:00:06 +0100

    Minor, unimportant change, just to test my git signing config works as expected

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
 
                 <p>I'm a Platform Engineer from Telford, UK</p><br/>
 
-                <p>Father of 4, husband of 1</p><br/>
+                <p>Father of 4, husband of 1, Liverpool fan</p><br/>
                 <p>This page is a work in progress, I'm using it to learn React development and just playing around, really.</p>
             
 
